fd128f12571d66e6770ece30a1c57752cf0b3196
Yitong Zhao <66962250+zhao-yitong@users.noreply.github.com>
2021-12-07 14:48:08 -0500

    Update urls.py

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -6,6 +6,7 @@ from rest_api import views
 urlpatterns = [
     url(r'^api/test/', views.test_list),
     url(r'^packages/',views.package_list),
+    url(r'^package/byName/<str:pk>/',views.package_by_name),
     path('package/',views.ModulePackageViewer.as_view(),name='ModulePackageViewer'),
     path('package/<str:pk>/',views.ModulePackageViewer.as_view(),name='ModulePackageViewer'),
     url(r'reset/',views.reset_registry)
