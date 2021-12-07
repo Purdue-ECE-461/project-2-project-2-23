5258da45e5b0f4078441b7fa49dbccc58e30267a
Yitong Zhao <66962250+zhao-yitong@users.noreply.github.com>
2021-12-07 12:07:16 -0500

    Adding contents to create new serializers

--- rest_api/serializers.py
+++ rest_api/serializers.py
@@ -19,8 +19,8 @@ class PackageMetaSerializer(serializers.Serializer):
     ID = serializers.CharField()
 
 class PackageDataSerializer(serializers.Serializer):
-    Content = serializers.CharField(required=False,default='')
-    URL = serializers.CharField(required=False,default='')
+    Content = serializers.CharField()
+    URL = serializers.CharField()
     JSProgram = serializers.CharField()
 
 class PackageCreationSerializer(WritableNestedModelSerializer):
@@ -30,3 +30,105 @@ class PackageCreationSerializer(WritableNestedModelSerializer):
     class Meta:
         model = ModulePackage
         fields = ('metadata','data')
+
+# Below are new added serilizers for package change 
+
+# Relating to field:
+# "User": {
+#       "name": "Paschal Amusuo",
+#       "isAdmin": true
+#     },
+
+class PackageChangeUserSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    isAdmin = serializers.BooleanField()
+
+# Relating to field:
+# "Date": "2021-11-21T01:11:11Z",
+
+class PackageChangeDateSerializer(serializers.Serializer):
+    Date = serializers.CharField()
+
+# Relating to field:
+# "PackageMetadata": {
+#       "Name": "Underscore",
+#       "Version": "1.0.0",
+#       "ID": "underscore"
+#     },
+# No need to create new serializer, using PackageMetaSerializer is OK
+
+
+# Relating to field:
+# "Action": "DOWNLOAD"
+
+class PackageChangeActionSerializer(serializers.Serializer):
+    Action = serializers.CharField()
+
+
+# Nested Serializer
+
+class PackageChangeSerializer(WritableNestedModelSerializer):
+    User = PackageChangeUserSerializer(source='*')
+    Date = PackageChangeDateSerializer(source='*')
+    PackageMetadata = PackageMetaSerializer(source='*')
+    Action = PackageChangeActionSerializer(source='*')
+
+    class Meta:
+        model = ModulePackage
+        fields = ('User','Date','PackageMetadata','Action')
+
+
+# [
+#   {
+#     "User": {
+#       "name": "Paschal Amusuo",
+#       "isAdmin": true
+#     },
+#     "Date": "2021-11-21T01:11:11Z",
+#     "PackageMetadata": {
+#       "Name": "Underscore",
+#       "Version": "1.0.0",
+#       "ID": "underscore"
+#     },
+#     "Action": "DOWNLOAD"
+#   },
+#   {
+#     "User": {
+#       "name": "Paschal Amusuo",
+#       "isAdmin": true
+#     },
+#     "Date": "2021-11-20T01:11:11Z",
+#     "PackageMetadata": {
+#       "Name": "Underscore",
+#       "Version": "1.0.0",
+#       "ID": "underscore"
+#     },
+#     "Action": "UPDATE"
+#   },
+#   {
+#     "User": {
+#       "name": "Paschal Amusuo",
+#       "isAdmin": true
+#     },
+#     "Date": "2021-11-19T01:11:11Z",
+#     "PackageMetadata": {
+#       "Name": "Underscore",
+#       "Version": "1.0.0",
+#       "ID": "underscore"
+#     },
+#     "Action": "RATE"
+#   },
+#   {
+#     "User": {
+#       "name": "Paschal Amusuo",
+#       "isAdmin": true
+#     },
+#     "Date": "2021-11-18T01:11:11Z",
+#     "PackageMetadata": {
+#       "Name": "Underscore",
+#       "Version": "1.0.0",
+#       "ID": "underscore"
+#     },
+#     "Action": "CREATE"
+#   }
+# ]
